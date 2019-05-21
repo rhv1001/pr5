35dd9f7562e0741bff00138df0999e15cf987ae6
Rubén Herrero <ruben@MacBook-Pro-de-Ruben.local>
2019-05-21 17:19:26 +0200

    activity_main quitado

--- app/src/main/java/com/example/pr5/MainActivity.java
+++ app/src/main/java/com/example/pr5/MainActivity.java
@@ -5,15 +5,6 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
 class MeteCuenta extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
