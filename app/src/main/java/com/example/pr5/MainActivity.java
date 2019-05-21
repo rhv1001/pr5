5274c83616fdb99421d70adf5724e60c6103750c
Rubén Herrero <ruben@MacBook-Pro-de-Ruben.local>
2019-05-21 17:26:50 +0200

    Cambio revertido

--- app/src/main/java/com/example/pr5/MainActivity.java
+++ app/src/main/java/com/example/pr5/MainActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
 class MeteCuenta extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
