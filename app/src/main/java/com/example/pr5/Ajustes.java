5a40b9b5ccb74acfefa26e4289547a1a8bdef450
pabloahita <pabloahitami@hotmail.es>
2019-05-21 23:38:05 +0200

    CAMBIOS

---
+++ app/src/main/java/com/example/pr5/Ajustes.java
@@ -0,0 +1,14 @@
+package com.example.pr5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.content.Intent;
+//import android.view.View;
+
+public class Ajustes extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ajustes);
+    }
+}
