e61b922ed61da882040c020e9ef266781306a57d
pabloahita <pabloahitami@hotmail.es>
2019-05-22 23:18:38 +0200

    ICONOS MENÚ AYUDA Y ACCESO AL FEEDBACK

--- app/src/main/java/com/example/pr5/Ayuda.java
+++ app/src/main/java/com/example/pr5/Ayuda.java
@@ -1,12 +1,18 @@
 package com.example.pr5;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Ayuda extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ayuda);
+    }
+    public void Feedback (View v){
+        Intent intent21 = new Intent (v.getContext(), Feedback.class);
+        startActivityForResult(intent21, 0);
     }
 }
